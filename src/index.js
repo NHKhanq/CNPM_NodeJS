963b7cf3a463117fd296aa4bcbae2f35693ecfa8
Thanhhhhhhhhhhh <truongvanhien123tv@gmail.com>
2024-05-24 01:26:59 +0700

    Cập nhật bổ sung

--- src/index.js
+++ src/index.js
@@ -11,11 +11,14 @@ const db = require('./config/db/index.js')
 db.connect()
 
 //import function controller
+const Product = require("./app/Controllers/ProductController.js")
+const Order = require("./app/Controllers/OrderController.js")
 const HomeController = require('./app/Controllers/HomeController')
 const LoginController =  require('./app/Controllers/LoginController')
+const OrderController =  require('./app/Controllers/OrderController')
 const SearchController = require('./app/Controllers/SearchController')
-const Product = require("./app/Controllers/ProductController.js")
 const ProductController = require("./app/Controllers/ProductController.js")
+const CreateController = require("./app/Controllers/CreateController.js")
 
 //static file
 app.use(express.static(path.join(__dirname, 'public')))
@@ -33,8 +36,12 @@ console.log('PATH: ', path.join(__dirname, 'resources/views'))
 //route
 app.get('/login',LoginController.login)
 app.get('/', HomeController.home)
-app.get('/search', SearchController.search)
 app.get('/Product/:slug', ProductController.detail)
+app.get('/search', SearchController.search)
+app.get('/create', CreateController.create)
+app.post('/create/post', CreateController.post)
+app.post('/order/buy', OrderController.buy)
+app.get('/order', OrderController.order)
 
 
 app.listen(port, () => {
