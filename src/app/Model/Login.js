9b2814c039daec48a75c419ee6e87536e1bc8e6a
TrongTranTVU <tranquoctrongtv@gmail.com>
2024-05-26 23:12:05 +0700

    Cập nhật bổ sung

--- src/app/Model/Login.js
+++ src/app/Model/Login.js
@@ -1,15 +1,9 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const Login = new Schema({
+const loginSchema = new Schema({
   username: { type: String, required: true },
-  password: { type: String, required: true },
-})
-function findByUsername(username) {
-  return users.find(user => user.username === username);
-}
+  password: { type: String, required: true }
+});
 
-module.exports = {
-  findByUsername
-};
-module.exports = mongoose.model('Login', Login);
+module.exports = mongoose.model('Login', loginSchema);
