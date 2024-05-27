291f4c5fac45789b2e8edf8acb9492d756e2b60a
NHKhanq <nhkhang061203@gmail.com>
2024-05-27 22:56:08 +0700

    Sửa lỗi upload hình ảnh, triển khai ứng dụng trên Docker và format code

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
+
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/nodejs-dev');
-        console.log("OK")
+        const DB_ENDPOINT = process.env.DB_ENDPOINT || "mongodb://localhost:27018/nodejs-dev";
+        await mongoose.connect(DB_ENDPOINT, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('Kết nối dữ liệu thành công!');
     } catch (error) {
-        console.log("NO OK")
-
+        console.error('Kết nối thất bại!', error);
     }
 }
 
-module.exports = { connect }
+module.exports = { connect };
